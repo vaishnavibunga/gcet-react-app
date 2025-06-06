ecf68908293a595a672fc92e1e3a2ae3e45d1019
VaishnaviBunga <vaishnavibunga09@gmail.com>
2025-06-06 12:38:13 +0530

    gvf

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,6 @@ import Header from "./components/Header";
 import Order from "./components/Order";
 import Footer from "./components/Footer";
 
-
-
 // Context
 export const AppContext = createContext();
 
